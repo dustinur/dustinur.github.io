3b8b4c929a6d14c4d1659fa5f4f9910706dcb4b1
dustinur <dustinuriah@gmail.com>
2018-02-11 15:13:32 -0700

    V2 DGM 2780 Bootstrap Assignment
    V2 DGM 2780 Bootstrap Assignment

--- dgm2780-Starter/js/main.js
+++ dgm2780-Starter/js/main.js
@@ -1,1 +1,12 @@
 
+
+// Remove fixed class from Nav and use Afix w/ JavaScript
+
+
+// $(document).ready(function(){
+//     $("#navbar").affix({
+//         offset: {
+//             top: $(".header").outerHeight(true)
+//         }
+//     });
+// });
